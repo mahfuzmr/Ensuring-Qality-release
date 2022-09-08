84313bc7a89dd937fe2061ebbf44107e79fcf29f
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-08 10:15:22 +0200

    Selenium search box trial

--- testautomation/selenium/intro-to-selenium.py
+++ testautomation/selenium/intro-to-selenium.py
@@ -5,3 +5,4 @@ from selenium import webdriver
 driver = webdriver.Chrome("C:/Users/m.rahman/Downloads/chromedriver_win32_105/chromedriver.exe")
 driver.get('http://automationpractice.com/index.php')
 driver.find_element_by_css_selector("input[id='search_query_top']").send_keys("t shirt")
+driver.find_element_by_css_selector("button[class='btn btn-default button-search']").click()
