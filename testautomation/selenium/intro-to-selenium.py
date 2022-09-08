03e4134272dd3a32c00b0d7eefb68476aa02ed53
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-08 10:32:25 +0200

    Used letes selenium syntax

--- testautomation/selenium/intro-to-selenium.py
+++ testautomation/selenium/intro-to-selenium.py
@@ -1,8 +1,9 @@
 # #!/usr/bin/env python
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Start the browser and navigate to http://automationpractice.com/index.php.
 driver = webdriver.Chrome("C:/Users/m.rahman/Downloads/chromedriver_win32_105/chromedriver.exe")
 driver.get('http://automationpractice.com/index.php')
-driver.find_element_by_css_selector("input[id='search_query_top']").send_keys("t shirt")
-driver.find_element_by_css_selector("button[class='btn btn-default button-search']").click()
+driver.find_element(By.ID,"search_query_top").send_keys("t shirt")
+driver.find_element(By.NAME,"submit_search").click()
