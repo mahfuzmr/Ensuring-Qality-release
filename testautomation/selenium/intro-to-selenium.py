1e3fc32cdcff718f2129fc863eb0005682ae8a1f
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-08 11:44:45 +0200

    search and count works with value added test

--- testautomation/selenium/intro-to-selenium.py
+++ testautomation/selenium/intro-to-selenium.py
@@ -4,6 +4,19 @@ from selenium.webdriver.common.by import By
 
 # Start the browser and navigate to http://automationpractice.com/index.php.
 driver = webdriver.Chrome("C:/Users/m.rahman/Downloads/chromedriver_win32_105/chromedriver.exe")
-driver.get('http://automationpractice.com/index.php')
-driver.find_element(By.ID,"search_query_top").send_keys("t shirt")
+url='http://automationpractice.com/index.php'
+search_item="dress"
+print("Navigating to "+ url)
+driver.get(url)
+print("Searcing item for "+search_item)
+driver.find_element(By.ID,"search_query_top").send_keys(search_item)
 driver.find_element(By.NAME,"submit_search").click()
+our_search=driver.find_element(By.CSS_SELECTOR,"div[id='center_column'] > h1 > span.lighter").text
+assert "DRESS" in our_search
+print("We are on th search page for "+our_search)
+result=driver.find_element(By.CSS_SELECTOR,"div[id='center_column'] > h1 > span.heading-counter").text
+if "7" in result:
+    print(result+" is accurate")
+else:
+     print(result+" is showing wrong result")
+
