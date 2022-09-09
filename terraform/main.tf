3e045711515c37fee0a9d7c22416aff9b6613e34
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-09 14:18:06 +0200

    modified according to udacity project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 provider "azurerm" {
   tenant_id       = "${var.tenant_id}"
   subscription_id = "${var.subscription_id}"
@@ -6,52 +5,50 @@ provider "azurerm" {
   client_secret   = "${var.client_secret}"
   features {}
 }
-
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tfstate64111"
+    container_name       = "tfstate"
+    key                  = ""
+    access_key           = "ScS6gbupU0cOi+3heMzfY6wUYkC+k3KkPovQ+Akbv9ZOEu8Kpl/m1rILZKaALSN7lv2lqgFSvxvx+AStV+zyng=="
+  }
+}
 module "resource_group" {
   source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
-#Network
 module "network" {
   source               = "./modules/network"
-  resource_group       = "${module.resource_group.resource_group_name}"
-  location             = "${var.location}"
-  virtual_network_name = "${var.virtual_network_name}"  
-  application_type     = "${var.application_type}"
   address_space        = "${var.address_space}"
-  resource_type        = "Network"
-  address_prefix       = "${var.address_prefix}"
-  
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
 }
-#Network security group
 
-module "nsg" {
-  source              = "./modules/nsg"
-  resource_group      = "${module.resource_group.resource_group_name}"
-  location            = "${var.location}"
-  application_type    = "${var.application_type}" 
-  subnet_id           = "${module.network.subnet_id_test}"
-  resource_type       = "NSG"
-  address_prefix      = "${var.address_prefix}"
-}
-#Public IP
-
-module "publicip" {
-  source           = "./modules/publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+module "nsg-test" {
+  source           = "./modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
-  resource_type    = "publicip"
-  
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
 }
-
-# Reference the AppService Module here.
 module "appservice" {
   source           = "./modules/appservice"
-  resource_group   = "${module.resource_group.resource_group_name}"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "publicip" {
+  source           = "./modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
 }
