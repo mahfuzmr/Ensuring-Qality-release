3de1762581c58f89bb17721811e2c5b869a674d8
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-09 15:16:40 +0200

    ssh key added

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,26 +1,28 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.public_ip}"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.test.id,]
   admin_ssh_key {
     username   = "mahfuzur"
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEJOA6iRkV/r6xLGTZuEMIvALoZXsNJ7ycbic6S9VUbrdbO25TTMEMWcHV4hB66GpgZpkmB4XgLLv50/RrbHN6l/ThJrF5ebj0rWyVpde79ZSz/1JBXOr4fYp7zCEbtPGKW8Jw8FQB82uFq3aHbXATlXh0r0tQrnFBUHUcBKlDFO0ugDrIUZ80du0BafhaXl2lnd3qKba9rNkwqsoxhAL6hiAUxx10KvFdSSdrnHwZy64KVBgvg264KqB3Yfr78PHJxwQwMXxr8/CwFPDB5OyVLDg2ZwHoVHE6570fKSzGD6Sm6RPOsU2WuXJGGv9XQ79NkrS87udJnMZ7/jICGW/U3H+0ap7fumlvvMXih65fTn/fKNciztcH8QOoUdNCs5KK4hoDuXUe+98KeA4dkXkxYw+AIZt/cI8cLtBIEZDLxktQ3XFhjDeFwRTzz+aVNUiIaRAwQOl2LjYFhexMeyK975M5dsQU640CFX7evwdX61jDWsgPd8DgJghGfR3iUx/5JNaJ4Qz6bpC0LP0PgbwLkB/jS1KNv84XcEOxFTcNGW4dsWtbEM+ArR/EcW3PX685GyfkhkHYde11FuzmT4CTGcFVnV11aBIUXAJM7tCpEuyjVXRc8t8urTO4wxlTFoEo65OWG1JDveBJZ7ijctF3htGWBjfsRSMVqzXafKe0aw== mahfuzur@cc-fac25d87-66fbc57944-ttk5q"
+    #file("./id_rsa.pub") 
+    #"file("~/.ssh/id_rsa.pub")"
   }
   os_disk {
     caching           = "ReadWrite"
@@ -29,7 +31,7 @@ resource "azurerm_linux_virtual_machine" "" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
