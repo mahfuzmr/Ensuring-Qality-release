c8ec240055f005086750b69c7915d70a6f0907f8
Mahfuzur Rahman <m.rahman@ramom.de>
2022-09-15 14:41:43 +0200

    update for pipeline integration

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -5,18 +5,19 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 import logging
 
-logging.basicConfig(filename="C:/logs/selenium/selenium_ui.log",format='%(asctime)s %(message)s',filemode='w') #/var/log/selenium/ui.log
+#logging.basicConfig(filename="C:/logs/selenium/selenium_ui.log",format='%(asctime)s %(message)s',filemode='w') #/var/log/selenium/selenium_ui.log
+logging.basicConfig(filename="/var/log/selenium/selenium_ui.log",format='%(asctime)s %(message)s',filemode='w') 
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)  
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)  
 
-    driver = webdriver.Chrome("C:/Users/m.rahman/Downloads/chromedriver_win32_105/chromedriver.exe")
+   #driver = webdriver.Chrome("C:/Users/m.rahman/Downloads/chromedriver_win32_105/chromedriver.exe")
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
